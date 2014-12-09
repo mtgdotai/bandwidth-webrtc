e8caa967a7c69a253277c3ae80aa56253702d520
Nathan Fuchs <fuchsnj@gmail.com>
2014-12-09 11:07:12 -0500

    added basic mocha test

--- test/unit/BWClient.js
+++ test/unit/BWClient.js
@@ -1,1 +1,9 @@
+"use strict";
 require("../../lib/BWClient");
+var assert = require("assert");
+
+describe("The Bandwidth WebRTC client", function () {
+	it("should declare BWClient as a global",function () {
+		assert(typeof BWClient !== undefined);
+	});
+});
