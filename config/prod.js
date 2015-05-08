44dacd94cd8d22c79bf9907f6402f42f01bfe888
Nathan Fuchs <fuchsnj@gmail.com>
2015-05-08 16:58:26 -0400

    remove google stun server, and port numbers

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,5 @@ module.exports = {
 	wsServers: [ "wss://webrtc.registration.bandwidth.com:10443" ],
 	incomingCallIcon: "https://d23gfec6haquvp.cloudfront.net/media/call_icon.png",
 	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3",
-	stunServers: [
-		"stun.registration.bandwidth.com:3478",
-		"stun1.l.google.com:19302"
-	]
+	stunServers: ["stun:stun.registration.bandwidth.com"]
 };
--- config/stage.js
+++ config/stage.js
@@ -3,8 +3,5 @@ module.exports = {
 	wsServers: [ "wss://stage-webrtc.registration.bandwidth.com:10443" ],
 	incomingCallIcon: "https://d23gfec6haquvp.cloudfront.net/media/call_icon.png",
 	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3",
-	stunServers: [
-		"stage-stun.registration.bandwidth.com:3478",
-		"stun1.l.google.com:19302"
-	]
+	stunServers: [ "stun:stage-stun.registration.bandwidth.com" ]
 };
