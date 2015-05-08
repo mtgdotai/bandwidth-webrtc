73b204fdfef31996cda162df904a0a82a84584c4
Nathan Fuchs <fuchsnj@gmail.com>
2015-05-08 16:46:53 -0400

    add stun servers

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,12 @@
 module.exports = {
 	ringAudio: "https://d23gfec6haquvp.cloudfront.net/media/ring.ogg",
-	wsServers: [ "wss://dev-webrtc.registration.bandwidth.com:10443" ],
+	wsServers: [
+		"wss://dev-webrtc.registration.bandwidth.com:10443"
+	],
 	incomingCallIcon: "https://d23gfec6haquvp.cloudfront.net/media/call_icon.png",
-	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3"
+	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3",
+	stunServers: [
+		"stage-stun.registration.bandwidth.com:3478",
+		"stun1.l.google.com:19302"
+	]
 };
--- config/stage.js
+++ config/stage.js
@@ -2,5 +2,9 @@ module.exports = {
 	ringAudio: "https://d23gfec6haquvp.cloudfront.net/media/ring.ogg",
 	wsServers: [ "wss://stage-webrtc.registration.bandwidth.com:10443" ],
 	incomingCallIcon: "https://d23gfec6haquvp.cloudfront.net/media/call_icon.png",
-	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3"
+	incomingCallAudio: "https://d23gfec6haquvp.cloudfront.net/media/incoming_alert.mp3",
+	stunServers: [
+		"stage-stun.registration.bandwidth.com:3478",
+		"stun1.l.google.com:19302"
+	]
 };
